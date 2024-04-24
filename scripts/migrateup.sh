729830de0295b6ce59e0adb1f491d76988c1db60
Silver Luhtoja <silver.luhtoja@gmail.com>
2024-04-24 19:28:49 +0000

    removing sec checks and migrate script update

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -2,4 +2,4 @@
 
 # read -t 5 -p "Migrate UP or DOWN: " option
 cd ./sql/schema;
-goose postgres postgres://test:test@localhost:25432/postgres up
+goose postgres postgres://test:test@localhost:25432/test up
