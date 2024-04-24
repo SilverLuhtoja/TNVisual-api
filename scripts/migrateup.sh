9fda8928cbd14a1193ef6e22724f581ba02768f4
Silver Luhtoja <silver.luhtoja@gmail.com>
2024-04-24 19:00:01 +0000

    goose and migrating

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -2,4 +2,4 @@
 
 # read -t 5 -p "Migrate UP or DOWN: " option
 cd ./sql/schema;
-goose postgres postgres://postgres:postgres@localhost:15432/postgres up
+goose postgres postgres://test:test@localhost:25432/postgres up
